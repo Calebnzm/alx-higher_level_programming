4551242e36d3038725603149fc36f01e09e90a03
root <calebnzioka2027@gmail.com>
2023-08-08 11:54:43 -0700

    0 correction still

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,21 +4,16 @@ number = random.randint(-10000, 10000)
 n = number
 last_ = int(str(number)[-1])
 last = last_
-try:
-    if last_ > 5:
-        if number > 0:
-            print(f"Last digit of {number} is {last_} and is greater than 5")
-        else:
-            print(f"Last digit of {number} is -{last_} and is greater than 5")
-    elif last_ == 0:
-        if number > 0:
-            print(f"Last digit of {number} is {last_} and is 0")
-        else:
-            print(f"Last digit of {number} is -{last_} and is 0")
-    elif last < 6 and last != 0:
-        if number > 0:
-            print(f"Last digit of {n} is {last} and is less than 6 and not 0")
-        else:
-            print(f"Last digit of {n} is -{last} and is less than 6 and not 0")
-except TypeError as e:
-    print(f"{e}")
+
+if last > 5 and number > 0:
+    print(f"Last digit of {number} is {last_} and is greater than 5")
+elif last > 5 and number < 0:
+    print(f"Last digit of {number} is -{last} and is less than 6 and not 0")
+elif last_ == 0:
+    print(f"Last digit of {number} is {last_} and is 0")
+elif last < 6 and last != 0 and number > 0:
+    print(f"Last digit of {n} is {last} and is less than 6 and not 0")
+elif last < 6 and last != 0 and number < 0:
+    print(f"Last digit of {n} is -{last} and is less than 6 and not 0")
+else:
+    print("Invalid case")
