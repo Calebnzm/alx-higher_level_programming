8b5ade4a95328f18226d6f52b2063b2b81c73927
root <calebnzioka2027@gmail.com>
2023-08-14 10:10:25 -0700

    4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    a = len(my_list)
+    if idx < 0 or idx > a:
+        return my_list
+    else:
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
