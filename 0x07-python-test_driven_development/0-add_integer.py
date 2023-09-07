da58ef0734d8686f6e32fecf3742d3b83ecd1bfb
root <calebnzioka2027@gmail.com>
2023-09-07 05:28:00 -0700

    Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+'''This is a module that defines an addition function'''
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    '''
+    Adds two integers or floats and returns the result as an integer.
+
+    Args:
+        a (int or float): The first number to be added.
+        b (int or float, optional): The second number to be added. Defaults to 98.
+
+    Returns:
+        int: The sum of a and b as an integer.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    '''
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    a = int(a)
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    b = int(b)
+
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+This is a directory for the 0x07-python-test_driven_development of the ALX SWE program higher level programming sprint.
