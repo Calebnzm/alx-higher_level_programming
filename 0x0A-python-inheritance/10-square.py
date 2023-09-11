1b22e54800bf1144d254d15615a9595c21094d51
root <calebnzioka2027@gmail.com>
2023-09-11 04:01:39 -0700

    task 10 correction

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 """a class Square that inherits from Rectangle (9-rectangle.py):"""
 
+class BaseGeometry:
+    """A class representing base geometry"""
+    def area(self):
+        """Raises an exception with the message 'area() is not implemented'"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """A class rectangle that inherits from BaseGeometry"""
