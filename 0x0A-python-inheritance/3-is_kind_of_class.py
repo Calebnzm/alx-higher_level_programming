5ff81f22f7bbe14f6c257c1e6e7774c46eedf46f
root <calebnzioka2027@gmail.com>
2023-09-11 02:59:42 -0700

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module checks if an object is a decsendant of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """This function checks if an object is a decsendant of a class"""
+    return isinstance(obj, a_class)
