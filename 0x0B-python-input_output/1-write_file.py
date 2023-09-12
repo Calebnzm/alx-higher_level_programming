bc545069ef5c0995fef0343f3f63393d146838db
root <calebnzioka2027@gmail.com>
2023-09-12 04:53:35 -0700

    task 1 correctio 0

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """WRItes to a file and returns no of characters writen"""
-    with open(file, mode='w', encoding='utf-8') as file:
+    with open(filename, mode='w', encoding='utf-8') as file:
         return file.write(text)
